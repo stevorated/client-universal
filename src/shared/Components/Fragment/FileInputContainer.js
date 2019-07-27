a17cbbff0f186f942884c76865c1e70c1b105c4e
stevorated <garbers8@gmail.com>
2019-07-27 17:27:48 +0300

    mobile upload

--- src/shared/Components/Fragment/FileInputContainer.js
+++ src/shared/Components/Fragment/FileInputContainer.js
@@ -1,14 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState, Fragment } from 'react'
 import { Container, Row, Col, Button } from 'reactstrap'
 import CropFileInput from './CropFileInput'
 import { connect } from 'react-redux'
 import { uploadFile } from '../../Store/actions'
 import { getCroppedImg } from '../../Utils'
+import Loading from './Loading';
 
 
 function FileInputContainer(props) {
   const reactCropPreview = document.getElementsByClassName('ReactCrop__image')
   const [valid, setValid] = useState(false)
+  const [loading, setLoading] = useState(false)
   const [error, setError] = useState('')
   const [fileData, setFileData] = useState({})
   const [preview, setPreview] = useState('')
@@ -29,12 +31,13 @@ function FileInputContainer(props) {
       const img = new Image()
       img.src = preview
 
-      img.onload = (e) => {
+      img.onload = async (e) => {
+        setLoading(true)
         const image = e.target
         const { unit, height, width, aspect, x, y } = crop
 
-        const scaleX = Math.round((image.naturalWidth / previewWidth)*10000)
-        const scaleY = Math.round((image.naturalHeight / previewHeight)*10000)
+        const scaleX = Math.round((image.naturalWidth / previewWidth) * 10000)
+        const scaleY = Math.round((image.naturalHeight / previewHeight) * 10000)
 
         const data = {
           ...fileData,
@@ -48,10 +51,10 @@ function FileInputContainer(props) {
           scaleY
         }
 
-        props.uploadFile(data)
+        await props.uploadFile(data).then(setTimeout(() => props.toggle(), 1000))
       }
-      props.toggle()
-      
+      // props.toggle()
+
     } else {
       setValid(false)
     }
@@ -59,27 +62,30 @@ function FileInputContainer(props) {
   }
   return (
     <Container >
-      <form onSubmit={handleSubmitAvatar}>
-        <CropFileInput
-          className="m-5"
-          {...props}
-          round={true}
-          valid={valid}
-          setValid={setValid}
-          error={error}
-          setError={setError}
-          setFileData={setFileData}
-          height={310}
-          width={310}
-          showText={true}
-          fileData={fileData}
-          setPreview={setPreview}
-          preview={preview}
-          setCrop={setCrop}
-          crop={crop}
-        />
-        <Button className="mt-2" disabled={!valid}>Confirm Upload</Button>
-      </form>
+      {loading ? <Loading /> : <Fragment >
+        <form onSubmit={handleSubmitAvatar}>
+          <CropFileInput
+            className="m-5"
+            {...props}
+            round={true}
+            valid={valid}
+            setValid={setValid}
+            error={error}
+            setError={setError}
+            setFileData={setFileData}
+            height={310}
+            width={310}
+            showText={true}
+            fileData={fileData}
+            setPreview={setPreview}
+            preview={preview}
+            setCrop={setCrop}
+            crop={crop}
+          />
+          <Button className="mt-2" disabled={!valid}>Confirm Upload</Button>
+        </form>
+      </Fragment>}
+
       <div id="divik"></div>
     </Container>
   )
--- src/shared/Store/actions/fileActions.js
+++ src/shared/Store/actions/fileActions.js
@@ -2,13 +2,10 @@ import { UPLOAD_FILE_TEST } from '../Apollo/Mutaions'
 
 export const uploadFile = (data) => async (dispatch, getState, client) => {
   const { file, size } = data
-  console.log('data', data)
-
   const res = await client.mutate({
     variables: data,
     mutation: UPLOAD_FILE_TEST
   })
-  console.log('res', res)
   const url = res.data.singleUpload.url
   dispatch({
     type: 'UPDATE_AVATAR',
