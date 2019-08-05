aad3d31cefcf77925fe1abdf66cc1d36a43f651e
stevorated <garbers8@gmail.com>
2019-08-05 13:38:42 +0300

    removed compose with dev tools for redux createStore in client and server

--- src/server/helpers/createStore.js
+++ src/server/helpers/createStore.js
@@ -1,15 +1,14 @@
-import { createStore , applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import reducers from '../../shared/Store/reducers'
-
+import { createStore, applyMiddleware } from "redux"
+import thunk from "redux-thunk"
+import { composeWithDevTools } from "redux-devtools-extension"
+import reducers from "../../shared/Store/reducers"
 
 export default client => {
   const store = createStore(
     reducers,
-    process.env.NODE_ENV === 'production' 
-  ? applyMiddleware(thunk.withExtraArgument(client))
-  : composeWithDevTools(applyMiddleware(thunk.withExtraArgument(client)))
-    )
+    process.env.NODE_ENV === "production"
+      ? applyMiddleware(thunk.withExtraArgument(client))
+      : composeWithDevTools(applyMiddleware(thunk.withExtraArgument(client)))
+  )
   return store
 }
